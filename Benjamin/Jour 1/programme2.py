555da6c4901cdecdd0d932512b81617fcd69eebc
lambertb <benjamin.lambert@edu.esiee.fr>
2022-01-24 14:17:22 +0100

    Début monopoly

---
+++ Benjamin/Jour 1/programme2.py
@@ -0,0 +1,11 @@
+from math import sqrt, log, exp, cos, sin, tan
+
+def facto(n):
+    f = 1
+    
+    for i in range(1, n+1):
+        f = f * i
+        
+    return f
+
+print(eval(str(input(""))))
