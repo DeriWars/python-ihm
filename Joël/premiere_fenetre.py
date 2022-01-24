fb8c516dcb940131d803b8275f47f31ba6ef75dd
mbiapakj <joeltresor.mbiapaketcheckmen@edu.esiee.fr>
2022-01-24 13:35:19 +0100

    bouton debut

--- Joël/premiere_fenetre.py
+++ Joël/premiere_fenetre.py
@@ -6,7 +6,10 @@ from PyQt5.QtWidgets import *
 app = QApplication(sys.argv)
 
 window = QWidget()
+window.setGeometry(0, 0, 200, 200)  # permet de postionner la fenetre et forcer la taille et le placement
+
+label = QLabel(window)  # on fait un label
+label.setText("Ma première appli")  # on lui mets du texte
 
 window.show()
-
 sys.exit(app.exec())
