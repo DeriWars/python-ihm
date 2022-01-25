dcedcfba4f3c1748b2078de331ff1c4a688d3827
lambertb <benjamin.lambert@edu.esiee.fr>
2022-01-25 18:09:20 +0100

    Without duplication

--- Benjamin/Appli/Projet 1/message_box.py
+++ Benjamin/Appli/Projet 1/message_box.py
@@ -1,42 +1,25 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 
+def box(icon, name, description, details=None, callback=None):
+    box = QMessageBox()  # create an instance of it
+    
+    box.setIcon(icon)  # set icon
+    box.setWindowTitle("Password manager - by Pékul") # set title
+    box.setText(name) # set text
+    box.setInformativeText(description) # set information under the main text
+    box.setStandardButtons(QMessageBox.Ok)  # type of buttons associated
+    
+    if details is not None: box.setDetailedText(details) # a button for more details will add in
+    if callback is not None: box.buttonClicked.connect(callback)  # connect clicked signal
+    
+    box.exec_()  # get the return value
+
 def info_box(name, description, callback=None):
-    info = QMessageBox()  # create an instance of it
-    
-    info.setIcon(QMessageBox.Information)  # set icon
-    info.setWindowTitle("Password manager - by Pékul") # set title
-    info.setText(name) # set text
-    info.setInformativeText(description) # set information under the main text
-    info.setStandardButtons(QMessageBox.Ok) # type of buttons associated
-    
-    if callback is not None: info.buttonClicked.connect(callback)  # connect clicked signal
-    info.exec_() # get the return value
+    box(QMessageBox.Information, name, description, callback=callback)
 
 def warn_box(name, description, details=None, callback=None):
-    info = QMessageBox()  # create an instance of it
-    
-    info.setIcon(QMessageBox.Warning) # set icon
-    info.setWindowTitle("Password manager - by Pékul") # set title
-    info.setText(name) # set text
-    info.setInformativeText(description) # set information under the main text
-    info.setStandardButtons(QMessageBox.Ok) # type of buttons associated
-    
-    if details is not None: info.setDetailedText(details) # a button for more details will add in
-    if callback is not None: info.buttonClicked.connect(callback)  # connect clicked signal
-    
-    info.exec_() # get the return value
+    box(QMessageBox.Warning, name, description, details=details, callback=callback)
 
 def error_box(name, description, details=None, callback=None):
-    info = QMessageBox()  # create an instance of it
-    
-    info.setIcon(QMessageBox.Critical)  # set icon
-    info.setWindowTitle("Password manager - by Pékul") # set title
-    info.setText(name) # set text
-    info.setInformativeText(description) # set information under the main text
-    info.setStandardButtons(QMessageBox.Ok) # type of buttons associated
-    
-    if details is not None: info.setDetailedText(details) # a button for more details will add in
-    if callback is not None: info.buttonClicked.connect(callback)  # connect clicked signal
-    
-    info.exec_() # get the return value
+    box(QMessageBox.Critical, name, description, details=details, callback=callback)
